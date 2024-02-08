a45232066b6c3e8937d300f64f12bfde59dd2d73
FrancioXavier <francioxaviersilva@gmail.com>
2024-02-08 09:51:50 -0300

    countAdjacentBombs e Play aula 08/02

--- minesweeper/countAdjacentBombs.asm
+++ minesweeper/countAdjacentBombs.asm
@@ -4,26 +4,39 @@
 
 countAdjacentBombs:
     save_context
+    move $s0, $a0
+    move $s1, $a1
+    move $s2, $a2
     
     li $t5, 0 #count = 0
-    sub $t1, $a0, 1 # i = row - 1
-    add $t2, $a0, 1 # t2 = row + 1
+    sub $t1, $s1, 1 # i = row - 1
+    add $t2, $s1, 1 # t2 = row + 1
     begin_for_i_it:
     bgt $t1, $t2, end_for_i_it
     
-    sub $t3, $a1, 1 # j = column - 1
-    add $t4, $a1, 1 # t4 = column + 1 
+    sub $t3, $s2, 1 # j = column - 1
+    add $t4, $s2, 1 # t4 = column + 1 
     begin_for_j_it:
     bgt $t3, $t4, end_for_j_it
     bge $t1, 0, else_invalid
     blt $t1, SIZE, else_invalid
     bge $t3, 0, else_invalid
     blt $t3, SIZE, else_invalid
-    # board[i][j] == -1 ???
+    # board[i][j] == -1
+    sll $t6, $a0, 4
+    sll $t7, $a1, 2
+    add $t6, $t6, $t7
+    add $t6, $t6, $s0
+    beq $t6, -1, else_invalid
+    
     add $t5, $t5, 1
     
-    jr $t5
-
+    else_invalid:
+    j begin_for_j_it
+    end_for_j_it:
+    j begin_for_i_it
+    end_for_i_it:
+    move $v0, $t5
     restore_context
     jr $ra
 
--- minesweeper/play.asm
+++ minesweeper/play.asm
@@ -4,13 +4,14 @@
 
 play:
     save_context
-    lw $t1, 0 ($t0) #perguntar se eh isso memo
-    li $t2, -1
-    beq $t1, $t2, return0
+    move $s0, $a0
 
-    li $t2, -2
+    li $t1, -1
+    beq $s0, $t0, return0
+
+    li $t1, -2
     jal countAdjacentBombs
-    sw $t3, 0 () # t3 == x
+    sw $t3, 0($v0) # t3 == x
     beq $t1, $t2, x_recb_count          #if ()
 
     li $t2, -1
@@ -20,7 +21,8 @@ play:
     jr $t2      #return 1
 
     return0:
-    jr $zero
+    li $v0, 0
+    syscall
 
     x_recb_count:
     move $t3, $t1
