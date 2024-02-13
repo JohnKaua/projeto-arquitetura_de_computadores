100e572775e60401a8ad477d0c85e2b71b63417a
JohnKaua <johnlima12342004@gmail.com>
2024-02-13 10:44:30 -0300

    update do play

--- minesweeper/play.asm
+++ minesweeper/play.asm
@@ -4,36 +4,38 @@
 
 play:
     save_context
+    li      $v0, 1                                  # valor de $v0 = 1
     
     sll		$t0, $s2, 5
     sll		$t1, $s3, 2
     add		$t0, $t0, $t1
-    add		$t0, $t0, $s0                           # $t0 = board[row][column]
+    add		$t0, $t0, $s0                           # $t0 = board[row][column] (endereço)
 
-    li $t2, -1
-    beq $t0, $t2, if_1                              # if(board[row][column] == -1)
+    lw      $t1, 0($t0)                             # $t1 = valor de $t0
 
-    li $t2, -2
-    beq $t0, $t2, if_2                              # if(board[row][column] == -2)
+    li      $t2, -1
+    beq     $t1, $t2, if_1                          # if(board[row][column] == -1)
+
+    li      $t2, -2
+    beq     $t1, $t2, if_2                          # if(board[row][column] == -2)
 
     if_1:
-        li $v0, 0                                   # return 0, game over
-        j end
+        li      $v0, 0                              # return 0, game over
+        j       end
 
     if_2:
-        jal countAdjacentBombs
-        move $t3, $v0                               # x = countAdjacentBombs
-        move $t0, $t3                               # board = x
+        jal     countAdjacentBombs
+        move    $t3, $v0                            # x = countAdjacentBombs
+        move    $t1, $t3                            # board = x
 
-        li $t2, 0                                   #                            !!!!!!!(falta arrumar)!!!!!!!
-        beq $t3, $t2, if_3                          # if(!x) revealAdjacentBombs !!!!!!!(falta arrumar)!!!!!!!
-        j end
+        li      $t2, 0                              #                            !!!!!!!(falta arrumar)!!!!!!!
+        beq     $t3, $t2, if_3                      # if(!x) revealAdjacentBombs !!!!!!!(falta arrumar)!!!!!!!
+        j       else_if_3
     
         if_3:
-            jal revealAdjacentBombs
-
-    li $v0, 1                                       # return 1, jogo continua
+            jal     revealAdjacentBombs
+    else_if_3:
+        li          $v0, 1                              # return 1
     end:
     restore_context
-    jr $ra
-    
+    jr      $ra
